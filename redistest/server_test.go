26edded951bc3e79c17f60c91e21aada75f45ae5
Satoshi Konno <skonno@cybergarage.org>
2022-07-26 21:16:34 +0900

    Add redistest.Server

---
+++ redistest/server_test.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2022 Satoshi Konno All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redistest
+
+import (
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	server := NewServer()
+
+	err := server.Start()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	client := NewClient()
+	err = client.Open()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = client.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = server.Stop()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
