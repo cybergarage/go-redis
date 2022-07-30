c3da43dfaf3ad9b78e6752a0da49feea932495b6
Satoshi Konno <skonno@cybergarage.org>
2022-07-31 00:14:41 +0900

    Update TestServer()

--- redistest/server_test.go
+++ redistest/server_test.go
@@ -36,26 +36,51 @@ func TestServer(t *testing.T) {
 
 	// ctx := context.Background()
 
-	msg := "Hello World!"
-	echo := client.Echo(msg)
-	if echo.Err() != nil {
-		t.Error(echo.Err())
-		return
-	}
-	if echo.Val() != msg {
-		t.Errorf("'%s' != '%s'", echo.Val(), msg)
-		return
-	}
+	t.Run("Echo", func(t *testing.T) {
+		msgs := []string{
+			"Hello World!",
+		}
+		for _, msg := range msgs {
+			t.Run(msg, func(t *testing.T) {
+				echo := client.Echo(msg)
+				if echo.Err() != nil {
+					t.Error(echo.Err())
+					return
+				}
+				if echo.Val() != msg {
+					t.Errorf("'%s' != '%s'", echo.Val(), msg)
+					return
+				}
+			})
+		}
+	})
 
-	err = client.Set("key", "value", 0).Err()
-	if err != nil {
-		t.Error(err)
-	}
+	t.Run("Set", func(t *testing.T) {
+		records := []struct {
+			key      string
+			val      string
+			expected string
+		}{
+			{"key", "value", "value"},
+		}
 
-	_, err = client.Get("key").Result()
-	if err != nil {
-		t.Error(err)
-	}
+		for _, r := range records {
+			t.Run(r.key+":"+r.val, func(t *testing.T) {
+				err = client.Set(r.key, r.val, 0).Err()
+				if err != nil {
+					t.Error(err)
+				}
+
+				res, err := client.Get(r.key).Result()
+				if err != nil {
+					t.Error(err)
+				}
+				if res != r.expected {
+					t.Errorf("%s != %s", res, r.expected)
+				}
+			})
+		}
+	})
 
 	// // panic: not implemented
 	// err = client.Quit().Err()
