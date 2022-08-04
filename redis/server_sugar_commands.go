9913990b2950e157759a40232a3a77d1d4c635e7
Satoshi Konno <skonno@cybergarage.org>
2022-08-04 21:12:29 +0900

    Support DECR command

--- redis/server_sugar_commands.go
+++ redis/server_sugar_commands.go
@@ -14,6 +14,8 @@
 
 package redis
 
+import "strconv"
+
 // nolint: gocyclo, maintidx
 func (server *Server) registerSugarExecutors() {
 	// Registers string commands.
@@ -38,5 +40,58 @@ func (server *Server) registerSugarExecutors() {
 			return nil, err
 		}
 		return NewIntegerMessage(len(newVal)), nil
+	})
+
+	incdecExecutor := func(ctx *DBContext, cmd string, key string, val int) (*Message, error) {
+		getOpt := GetOption{}
+		getRet, err := server.userCommandHandler.Get(ctx, key, getOpt)
+		if err != nil {
+			return nil, err
+		}
+		currVal := 0
+		if !getRet.IsNil() {
+			retVal, err := getRet.Integer()
+			if err != nil {
+				return nil, err
+			}
+			currVal = retVal
+		}
+		newVal := currVal + val
+		opt := newDefaultSetOption()
+		_, err = server.userCommandHandler.Set(ctx, key, strconv.Itoa(newVal), opt)
+		if err != nil {
+			return nil, err
+		}
+		return NewIntegerMessage(newVal), nil
+	}
+
+	server.RegisterExexutor("APPEND", func(ctx *DBContext, cmd string, args Arguments) (*Message, error) {
+		key, appendVal, err := nextSetArguments(cmd, args)
+		if err != nil {
+			return nil, err
+		}
+		getOpt := GetOption{}
+		getRet, err := server.userCommandHandler.Get(ctx, key, getOpt)
+		if err != nil {
+			return nil, err
+		}
+		newVal := appendVal
+		if getVal, err := getRet.String(); err == nil {
+			newVal = getVal + appendVal
+		}
+		opt := newDefaultSetOption()
+		_, err = server.userCommandHandler.Set(ctx, key, newVal, opt)
+		if err != nil {
+			return nil, err
+		}
+		return NewIntegerMessage(len(newVal)), nil
+	})
+
+	server.RegisterExexutor("DECR", func(ctx *DBContext, cmd string, args Arguments) (*Message, error) {
+		key, err := nextKeyArgument(cmd, args)
+		if err != nil {
+			return nil, err
+		}
+		return incdecExecutor(ctx, cmd, key, -1)
 	})
 }
