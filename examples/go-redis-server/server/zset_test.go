1cbd4db67336f7e258cf0af7bcf62a78ca86168f
Satoshi Konno <skonno@cybergarage.org>
2022-09-02 01:07:02 +0900

    Add TestZSet

---
+++ examples/go-redis-server/server/zset_test.go
@@ -0,0 +1,31 @@
+// Copyright (C) 2022 The go-redis Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"testing"
+)
+
+func TestZSet(t *testing.T) {
+	testCases := []struct {
+		member   string
+		expected []string
+	}{}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.member, func(t *testing.T) {
+		})
+	}
+}
