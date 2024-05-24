5ac6766a4abe19386985aa04be3ed6ea2202cc47
Satoshi Konno <skonno@cybergarage.org>
2024-05-24 18:03:22 +0900

    Add auth manager package

---
+++ redis/auth/conn.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2019 The go-redis Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"crypto/tls"
+	"net"
+)
+
+// Conn represents a connection.
+type Conn interface {
+	net.Conn
+
+	// User returns the user name and true if the connection has the user name.
+	User() (string, bool)
+	// Password returns the password and true if the connection has the password.
+	Password() (string, bool)
+
+	// IsTLSConnection return true if the connection is enabled TLS.
+	IsTLSConnection() bool
+	// TLSConnectionState returns the TLS connection state.
+	TLSConnectionState() (*tls.ConnectionState, bool)
+}
---
+++ redis/auth/manager.go
@@ -0,0 +1,52 @@
+// Copyright (C) 2019 The go-redis Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+// AuthManager represent an authenticator manager.
+type AuthManager struct {
+	authenticators []Authenticator
+}
+
+// NewAuthManager returns a new authenticator manager.
+func NewAuthManager() *AuthManager {
+	manager := &AuthManager{
+		authenticators: make([]Authenticator, 0),
+	}
+	return manager
+}
+
+// AddAuthenticator adds a new authenticator.
+func (mgr *AuthManager) AddAuthenticator(authenticator Authenticator) {
+	mgr.authenticators = append(mgr.authenticators, authenticator)
+}
+
+// ClearAuthenticators clears all authenticators.
+func (mgr *AuthManager) ClearAuthenticators() {
+	mgr.authenticators = make([]Authenticator, 0)
+}
+
+// Authenticate authenticates the connection with the startup message.
+func (mgr *AuthManager) Authenticate(conn Conn) (bool, error) {
+	if len(mgr.authenticators) == 0 {
+		return true, nil
+	}
+	for _, authenticator := range mgr.authenticators {
+		ok, err := authenticator.Authenticate(conn)
+		if !ok {
+			return false, err
+		}
+	}
+	return true, nil
+}
