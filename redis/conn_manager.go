cbfc7305672aafde401d8ed4b850e544bea2a877
Satoshi Konno <skonno@cybergarage.org>
2024-06-10 14:40:12 +0400

    Add ConnManager

---
+++ redis/conn_manager.go
@@ -0,0 +1,65 @@
+// Copyright (C) 2022 The go-redis Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redis
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+// ConnManager represents a connection map.
+type ConnManager struct {
+	m     map[uuid.UUID]*Conn
+	mutex *sync.RWMutex
+}
+
+// NewConnManager returns a connection map.
+func NewConnManager() *ConnManager {
+	return &ConnManager{
+		m:     map[uuid.UUID]*Conn{},
+		mutex: &sync.RWMutex{},
+	}
+}
+
+// AddConn adds the specified connection.
+func (mgr *ConnManager) AddConn(c *Conn) {
+	mgr.mutex.Lock()
+	defer mgr.mutex.Unlock()
+	uuid := c.UUID()
+	mgr.m[uuid] = c
+}
+
+// UUID returns a connection and true when the specified connection exists by the connection ID, otherwise nil and false.
+func (mgr *ConnManager) GetConnByUUID(uuid uuid.UUID) (*Conn, bool) {
+	mgr.mutex.RLock()
+	defer mgr.mutex.RUnlock()
+	c, ok := mgr.m[uuid]
+	return c, ok
+}
+
+// DeleteConnByUID deletes the specified connection by the connection ID.
+func (mgr *ConnManager) DeleteConnByUID(uuid uuid.UUID) {
+	mgr.mutex.Lock()
+	defer mgr.mutex.Unlock()
+	delete(mgr.m, uuid)
+}
+
+// Length returns the included connection count.
+func (mgr *ConnManager) Length() int {
+	mgr.mutex.RLock()
+	defer mgr.mutex.RUnlock()
+	return len(mgr.m)
+}
